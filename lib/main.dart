e60fbb2a2729f3d34e670b857510df34c2ba9fec
Papa Kofi <humblepapa1234@gmail.com>
2023-06-17 16:50:01 +0000

    Id card using stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,88 @@
 import 'package:flutter/material.dart';
+import "package:simple_icons/simple_icons.dart";
 
 void main() {
   runApp(const IDcard());
 }
 
 List<Widget> columnChildren = [
-  const Text('NAME',
+  const Center(
+    child: CircleAvatar(
+      backgroundImage: AssetImage('assets/papakofi.jpg'),
+      radius: 50.0,
+    ),
+  ),
+  const SizedBox(
+    height: 10.0,
+  ),
+  const Divider(
+    height: 20.0,
+    thickness: 1.0,
+    color: Colors.grey,
+  ),
+  const SizedBox(
+    height: 10.0,
+  ),
+  const Text('NAME:',
       style: TextStyle(
         color: Colors.black87,
         letterSpacing: 2.0,
         fontSize: 16.0,
         fontWeight: FontWeight.bold,
       )),
-  const Text('Papa Kofi Boahen',
-      style: TextStyle(
+  const SizedBox(
+    height: 10.0,
+  ),
+  const Text(
+    'Papa Kofi Boahen',
+    style: TextStyle(
+      color: Colors.black87,
+      letterSpacing: 2.0,
+      fontSize: 30.0,
+      fontWeight: FontWeight.bold,
+    ),
+  ),
+  const SizedBox(
+    height: 20.0,
+  ),
+  const Row(
+    children: <Widget>[
+      Icon(
+        Icons.email,
         color: Colors.black87,
-        letterSpacing: 2.0,
-        fontSize: 20.0,
-        fontWeight: FontWeight.bold,
-      )),
+      ),
+      SizedBox(
+        width: 10.0,
+      ),
+      Text('papakofiboahen@gmail.com', style: TextStyle(fontSize: 20.0)),
+    ],
+  ),
+  const SizedBox(height: 10.0),
+  const Row(
+    children: <Widget>[
+      Icon(
+        SimpleIcons.github,
+        color: Colors.black87,
+      ),
+      SizedBox(
+        width: 10.0,
+      ),
+      Text('Boahen123', style: TextStyle(fontSize: 20.0)),
+    ],
+  ),
+  const SizedBox(height: 10.0),
+  const Row(
+    children: <Widget>[
+      Icon(
+        SimpleIcons.linkedin,
+        color: Colors.black87,
+      ),
+      SizedBox(
+        width: 10.0,
+      ),
+      Text('Papa Kofi Boahen', style: TextStyle(fontSize: 20.0)),
+    ],
+  ),
 ];
 
 class IDcard extends StatelessWidget {
@@ -36,8 +100,10 @@ class IDcard extends StatelessWidget {
           elevation: 0.0,
         ),
         body: Container(
-          padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-          child: Column(children: columnChildren),
+          padding: const EdgeInsets.fromLTRB(40.0, 50.0, 30.0, 0.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: columnChildren),
         ),
       ),
     );
