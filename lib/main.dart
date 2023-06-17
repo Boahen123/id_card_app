57b81d4608328cf3e6e44ecde42dd6969f542251
Papa Kofi <humblepapa1234@gmail.com>
2023-06-17 06:14:31 +0000

    beginning template of an app

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,40 @@ void main() {
   runApp(const IDcard());
 }
 
+List<Widget> columnChildren = [
+  const Text('NAME',
+      style: TextStyle(
+        color: Colors.black87,
+        letterSpacing: 2.0,
+        fontSize: 16.0,
+        fontWeight: FontWeight.bold,
+      )),
+  const Text('Papa Kofi Boahen',
+      style: TextStyle(
+        color: Colors.black87,
+        letterSpacing: 2.0,
+        fontSize: 20.0,
+        fontWeight: FontWeight.bold,
+      )),
+];
+
 class IDcard extends StatelessWidget {
   const IDcard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        body: Center(
-          child: Text('Hello World'),
+        backgroundColor: Colors.grey[400],
+        appBar: AppBar(
+          title: const Text('Identity Card', style: TextStyle(fontSize: 25.0)),
+          centerTitle: true,
+          backgroundColor: Colors.black87,
+          elevation: 0.0,
+        ),
+        body: Container(
+          padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(children: columnChildren),
         ),
       ),
     );
